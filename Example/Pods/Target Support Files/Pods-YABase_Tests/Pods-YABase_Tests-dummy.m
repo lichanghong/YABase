4e6ccf5758df96d7f23f549ba0d198e2cf48cc2b
lich7 <lich7@ziroom.com>
2022-03-19 23:27:45 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-YABase_Tests/Pods-YABase_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YABase_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_YABase_Tests
+@end
---
+++ Example/Pods/Target Support Files/YABase/YABase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YABase : NSObject
+@end
+@implementation PodsDummy_YABase
+@end
